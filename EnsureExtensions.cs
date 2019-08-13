b9c59d7ad3e36089e9f686d4d7c129108151d4f2
Константин Дьяченко <konard@me.com>
2019-08-13 16:12:21 +0300

    Closes #9.

---
+++ EnsureExtensions.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using Platform.Exceptions;
+using Platform.Exceptions.ExtensionRoots;
+
+#pragma warning disable IDE0060 // Remove unused parameter
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace Platform.Reflection
+{
+
+    public static class EnsureExtensions
+    {
+        #region Always
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void IsUnsignedInteger<T>(this EnsureAlwaysExtensionRoot root, Func<string> messageBuilder)
+        {
+            if (!CachedTypeInfo<T>.IsNumeric || CachedTypeInfo<T>.IsSigned || CachedTypeInfo<T>.IsFloatPoint)
+            {
+                throw new NotSupportedException(messageBuilder());
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void IsUnsignedInteger<T>(this EnsureAlwaysExtensionRoot root, string message)
+        {
+            string messageBuilder() => message;
+            IsUnsignedInteger<T>(root, messageBuilder);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void IsUnsignedInteger<T>(this EnsureAlwaysExtensionRoot root) => IsUnsignedInteger<T>(root, (string)null);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void IsSignedInteger<T>(this EnsureAlwaysExtensionRoot root, Func<string> messageBuilder)
+        {
+            if (!CachedTypeInfo<T>.IsNumeric || !CachedTypeInfo<T>.IsSigned || CachedTypeInfo<T>.IsFloatPoint)
+            {
+                throw new NotSupportedException(messageBuilder());
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void IsSignedInteger<T>(this EnsureAlwaysExtensionRoot root, string message)
+        {
+            string messageBuilder() => message;
+            IsSignedInteger<T>(root, messageBuilder);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void IsSignedInteger<T>(this EnsureAlwaysExtensionRoot root) => IsSignedInteger<T>(root, (string)null);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void IsSigned<T>(this EnsureAlwaysExtensionRoot root, Func<string> messageBuilder)
+        {
+            if (!CachedTypeInfo<T>.IsSigned)
+            {
+                throw new NotSupportedException(messageBuilder());
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void IsSigned<T>(this EnsureAlwaysExtensionRoot root, string message)
+        {
+            string messageBuilder() => message;
+            IsSigned<T>(root, messageBuilder);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void IsSigned<T>(this EnsureAlwaysExtensionRoot root) => IsSigned<T>(root, (string)null);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void IsNumeric<T>(this EnsureAlwaysExtensionRoot root, Func<string> messageBuilder)
+        {
+            if (!CachedTypeInfo<T>.IsNumeric)
+            {
+                throw new NotSupportedException(messageBuilder());
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void IsNumeric<T>(this EnsureAlwaysExtensionRoot root, string message)
+        {
+            string messageBuilder() => message;
+            IsNumeric<T>(root, messageBuilder);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void IsNumeric<T>(this EnsureAlwaysExtensionRoot root) => IsNumeric<T>(root, (string)null);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void CanBeNumeric<T>(this EnsureAlwaysExtensionRoot root, Func<string> messageBuilder)
+        {
+            if (!CachedTypeInfo<T>.CanBeNumeric)
+            {
+                throw new NotSupportedException(messageBuilder());
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void CanBeNumeric<T>(this EnsureAlwaysExtensionRoot root, string message)
+        {
+            string messageBuilder() => message;
+            CanBeNumeric<T>(root, messageBuilder);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void CanBeNumeric<T>(this EnsureAlwaysExtensionRoot root) => CanBeNumeric<T>(root, (string)null);
+
+        #endregion
+
+        #region OnDebug
+
+        [Conditional("DEBUG")]
+        public static void IsUnsignedInteger<T>(this EnsureOnDebugExtensionRoot root, Func<string> messageBuilder) => Ensure.Always.IsUnsignedInteger<T>(messageBuilder);
+
+        [Conditional("DEBUG")]
+        public static void IsUnsignedInteger<T>(this EnsureOnDebugExtensionRoot root, string message) => Ensure.Always.IsUnsignedInteger<T>(message);
+
+        [Conditional("DEBUG")]
+        public static void IsUnsignedInteger<T>(this EnsureOnDebugExtensionRoot root) => Ensure.Always.IsUnsignedInteger<T>();
+
+        [Conditional("DEBUG")]
+        public static void IsSignedInteger<T>(this EnsureOnDebugExtensionRoot root, Func<string> messageBuilder) => Ensure.Always.IsSignedInteger<T>(messageBuilder);
+
+        [Conditional("DEBUG")]
+        public static void IsSignedInteger<T>(this EnsureOnDebugExtensionRoot root, string message) => Ensure.Always.IsSignedInteger<T>(message);
+
+        [Conditional("DEBUG")]
+        public static void IsSignedInteger<T>(this EnsureOnDebugExtensionRoot root) => Ensure.Always.IsSignedInteger<T>();
+
+        [Conditional("DEBUG")]
+        public static void IsSigned<T>(this EnsureOnDebugExtensionRoot root, Func<string> messageBuilder) => Ensure.Always.IsSigned<T>(messageBuilder);
+
+        [Conditional("DEBUG")]
+        public static void IsSigned<T>(this EnsureOnDebugExtensionRoot root, string message) => Ensure.Always.IsSigned<T>(message);
+
+        [Conditional("DEBUG")]
+        public static void IsSigned<T>(this EnsureOnDebugExtensionRoot root) => Ensure.Always.IsSigned<T>();
+
+        [Conditional("DEBUG")]
+        public static void IsNumeric<T>(this EnsureOnDebugExtensionRoot root, Func<string> messageBuilder) => Ensure.Always.IsNumeric<T>(messageBuilder);
+
+        [Conditional("DEBUG")]
+        public static void IsNumeric<T>(this EnsureOnDebugExtensionRoot root, string message) => Ensure.Always.IsNumeric<T>(message);
+
+        [Conditional("DEBUG")]
+        public static void IsNumeric<T>(this EnsureOnDebugExtensionRoot root) => Ensure.Always.IsNumeric<T>();
+
+        [Conditional("DEBUG")]
+        public static void CanBeNumeric<T>(this EnsureOnDebugExtensionRoot root, Func<string> messageBuilder) => Ensure.Always.CanBeNumeric<T>(messageBuilder);
+
+        [Conditional("DEBUG")]
+        public static void CanBeNumeric<T>(this EnsureOnDebugExtensionRoot root, string message) => Ensure.Always.CanBeNumeric<T>(message);
+
+        [Conditional("DEBUG")]
+        public static void CanBeNumeric<T>(this EnsureOnDebugExtensionRoot root) => Ensure.Always.CanBeNumeric<T>();
+
+        #endregion
+    }
+}
