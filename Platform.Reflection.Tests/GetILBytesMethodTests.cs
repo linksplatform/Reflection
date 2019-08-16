2f5395a6592fc3dc43c00e3cc18a8077fbd28c7c
Константин Дьяченко <konard@me.com>
2019-08-16 11:07:23 +0300

    Added GetILBytesMethodTests moved from https://github.com/Konard/LinksPlatform/blob/31dc8c6cc2e853f6e0dc618d6e3f0e771c1802a6/Platform/Platform.Tests/Reflection/ReflectionTests.cs

---
+++ Platform.Reflection.Tests/GetILBytesMethodTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using Xunit;
+using Platform.Collections;
+using Platform.Collections.Lists;
+
+namespace Platform.Reflection.Tests
+{
+    public static class GetILBytesMethodTests
+    {
+        [Fact]
+        public static void ILBytesForDelegateAreAvailableTest()
+        {
+            var x = new Func<object, int>(y => 0);
+            var bytes = x.GetMethodInfo().GetILBytes();
+            Assert.False(bytes.IsNullOrEmpty());
+        }
+
+        [Fact]
+        public static void ILBytesForDifferentDelegatesAreTheSameTest()
+        {
+            var x = new Func<object, int>(y => 0);
+            var z = new Func<object, int>(y => 0);
+            Assert.False(x == z);
+            var xBytes = x.GetMethodInfo().GetILBytes();
+            Assert.False(xBytes.IsNullOrEmpty());
+            var zBytes = x.GetMethodInfo().GetILBytes();
+            Assert.False(zBytes.IsNullOrEmpty());
+            Assert.True(xBytes.EqualTo(zBytes));
+        }
+    }
+}
