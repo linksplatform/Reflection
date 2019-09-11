b653a7121e7f269b661afc2dbfc7e6867b9d455d
Konstantin Dyachenko <konard@me.com>
2019-09-11 15:17:13 +0300

    Update push-nuget.sh

--- push-nuget.sh
+++ push-nuget.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-status="$(curl -Is https://www.nuget.org/packages/Platform.${TRAVIS_REPO_NAME}/0.1.0 | head -1)"
-validate=( $status )
-if [ ${validate[-2]} == "200" ]; then
-  echo "NuGet with current version is already pushed."
-  exit 0
-fi
+#status="$(curl -Is https://www.nuget.org/packages/Platform.${TRAVIS_REPO_NAME}/0.1.0 | head -1)"
+#validate=( $status )
+#if [ ${validate[-2]} == "200" ]; then
+#  echo "NuGet with current version is already pushed."
+#  exit 0
+#fi
 
 dotnet pack -c Release
+
+ls Platform.${TRAVIS_REPO_NAME}/bin/Release/netstandard*/
+
 dotnet nuget push -s https://api.nuget.org/v3/index.json -k ${NUGETTOKEN} **/*.nupkg
+
+# Clean up
 find . -type f -name '*.nupkg' -delete
 find . -type f -name '*.snupkg' -delete
