59539489c89d815d230d482a7346af6578db52c1
Konstantin Dyachenko <konard@me.com>
2019-09-11 13:45:29 +0300

    Update push-nuget.sh

--- push-nuget.sh
+++ push-nuget.sh
@@ -1,5 +1,7 @@
-nuget SetApiKey ${NUGETTOKEN}
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
 dotnet pack -c Release
-dotnet nuget push -s https://api.nuget.org/v3/index.json **/*.nupkg
-del /s /q *.nupkg
-del /s /q *.snupkg
+dotnet nuget push -s https://api.nuget.org/v3/index.json -k ${NUGETTOKEN} **/*.nupkg
+find . -type f -name '*.nupkg' -delete
+find . -type f -name '*.snupkg' -delete
