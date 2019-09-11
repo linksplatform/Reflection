55710389e487359e76565f0316897372b9a25300
Konstantin Dyachenko <konard@me.com>
2019-09-11 16:08:28 +0300

    Do not push, if the page of the package's version already exists.

--- push-nuget.sh
+++ push-nuget.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-#status="$(curl -Is https://www.nuget.org/packages/Platform.${TRAVIS_REPO_NAME}/0.1.0 | head -1)"
-#validate=( $status )
-#if [ ${validate[-2]} == "200" ]; then
-#  echo "NuGet with current version is already pushed."
-#  exit 0
-#fi
-
+# Pack NuGet package
 dotnet pack -c Release
 
 # Get version string
 PackageFileNamePrefix="Platform.${TRAVIS_REPO_NAME}/bin/Release/Platform.${TRAVIS_REPO_NAME}."
 PackageFileNameSuffix=".nupkg"
 PackageFileName=$(echo ${PackageFileNamePrefix}*${PackageFileNameSuffix})
-echo $PackageFileName
 Version="${PackageFileName#$PackageFileNamePrefix}"
 Version="${Version%$PackageFileNameSuffix}"
-echo $Version
 
+# Ensure NuGet package does not exist
+NuGetPageStatus="$(curl -Is https://www.nuget.org/packages/Platform.${TRAVIS_REPO_NAME}/${Version} | head -1)"
+StatusContents=( $NuGetPageStatus )
+if [ ${StatusContents[-2]} == "200" ]; then
+  echo "NuGet with current version is already pushed."
+  exit 0
+fi
+
+# Push NuGet package
 dotnet nuget push -s https://api.nuget.org/v3/index.json -k ${NUGETTOKEN} **/*.nupkg
 
 # Clean up
