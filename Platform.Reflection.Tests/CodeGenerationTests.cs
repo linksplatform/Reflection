f33c614e84c93adb805a2b5a18f470cbd08098f1
Константин Дьяченко <konard@me.com>
2019-09-24 00:23:49 +0300

    Added alternative way for building the delegate, but the performance comparison does not give a conclusive result.

--- Platform.Reflection.Tests/CodeGenerationTests.cs
+++ Platform.Reflection.Tests/CodeGenerationTests.cs
@@ -1,13 +1,19 @@
-﻿using System;
-using System.Reflection.Emit;
+﻿using Platform.Diagnostics;
+using System;
+using System.Runtime.CompilerServices;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Platform.Reflection.Tests
 {
-    public static class CodeGenerationTests
+    public class CodeGenerationTests
     {
+        private readonly ITestOutputHelper _output;
+
+        public CodeGenerationTests(ITestOutputHelper output) => _output = output;
+
         [Fact]
-        public static void EmptyActionCompilationTest()
+        public void EmptyActionCompilationTest()
         {
             var compiledAction = DelegateHelpers.Compile<Action>(generator =>
             {
@@ -17,7 +23,7 @@ namespace Platform.Reflection.Tests
         }
 
         [Fact]
-        public static void FailedActionCompilationTest()
+        public void FailedActionCompilationTest()
         {
             var compiledAction = DelegateHelpers.Compile<Action>(generator =>
             {
@@ -27,7 +33,7 @@ namespace Platform.Reflection.Tests
         }
 
         [Fact]
-        public static void ConstantLoadingTest()
+        public void ConstantLoadingTest()
         {
             CheckConstantLoading<byte>(8);
             CheckConstantLoading<uint>(8);
@@ -35,14 +41,83 @@ namespace Platform.Reflection.Tests
             CheckConstantLoading<ulong>(8);
         }
 
-        private static void CheckConstantLoading<T>(T value)
+        private void CheckConstantLoading<T>(T value)
         {
             var compiledFunction = DelegateHelpers.Compile<Func<T>>(generator =>
             {
-                generator.LoadConstant<T>(value);
+                generator.LoadConstant(value);
                 generator.Return();
             });
             Assert.Equal(value, compiledFunction());
+        }
+
+        private class MethodsContainer
+        {
+            public static readonly Func<int> DelegateWithoutAggressiveInlining;
+            public static readonly Func<int> DelegateWithAggressiveInlining;
+
+            static MethodsContainer()
+            {
+                void emitCode(System.Reflection.Emit.ILGenerator generator)
+                {
+                    generator.LoadConstant(72893);
+                    generator.LoadConstant(67421);
+                    generator.Add();
+                    generator.Return();
+                };
+                DelegateWithoutAggressiveInlining = DelegateHelpers.Compile<Func<int>>(emitCode, aggressiveInlining: false);
+                DelegateWithAggressiveInlining = DelegateHelpers.Compile<Func<int>>(emitCode, aggressiveInlining: true);
+            }
+            
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static int WrapperForDelegateWithoutAggressiveInlining() => DelegateWithoutAggressiveInlining();
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static int WrapperForDelegateWithAggressiveInlining() => DelegateWithAggressiveInlining();
+        }
+
+        [Fact]
+        public void AggressiveInliningEffectTest()
+        {
+            const int N = 100000000;
+        
+            int result = 0;
+
+            var ts4 = Performance.Measure(() =>
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    result = MethodsContainer.WrapperForDelegateWithAggressiveInlining();
+                }
+            });
+
+            var ts1 = Performance.Measure(() =>
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    result = MethodsContainer.DelegateWithoutAggressiveInlining();
+                }
+            });
+
+            // Currently fastest
+            var ts2 = Performance.Measure(() =>
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    result = MethodsContainer.DelegateWithAggressiveInlining();
+                }
+            });
+
+            var ts3 = Performance.Measure(() =>
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    result = MethodsContainer.WrapperForDelegateWithoutAggressiveInlining();
+                }
+            });
+
+            var output = $"{ts1} {ts2} {ts3} {ts4} {result}";
+            _output.WriteLine(output);
         }
     }
 }
--- Platform.Reflection/DelegateHelpers.cs
+++ Platform.Reflection/DelegateHelpers.cs
@@ -11,13 +11,13 @@ namespace Platform.Reflection
 {
     public static class DelegateHelpers
     {
-        public static TDelegate CompileOrDefault<TDelegate>(Action<ILGenerator> emitCode)
+        public static TDelegate CompileOrDefault<TDelegate>(Action<ILGenerator> emitCode, bool aggressiveInlining)
             where TDelegate : Delegate
         {
             var @delegate = default(TDelegate);
             try
             {
-                @delegate = CompileUsingDynamicMethod<TDelegate>(emitCode);
+                @delegate = aggressiveInlining ? CompileUsingMethodBuilder<TDelegate>(emitCode) : CompileUsingDynamicMethod<TDelegate>(emitCode);
             }
             catch (Exception exception)
             {
@@ -26,16 +26,20 @@ namespace Platform.Reflection
             return @delegate;
         }
 
-        public static TDelegate Compile<TDelegate>(Action<ILGenerator> emitCode)
+        public static TDelegate CompileOrDefault<TDelegate>(Action<ILGenerator> emitCode) where TDelegate : Delegate => CompileOrDefault<TDelegate>(emitCode, false);
+
+        public static TDelegate Compile<TDelegate>(Action<ILGenerator> emitCode, bool aggressiveInlining)
             where TDelegate : Delegate
         {
-            var @delegate = CompileOrDefault<TDelegate>(emitCode);
+            var @delegate = CompileOrDefault<TDelegate>(emitCode, aggressiveInlining);
             if (EqualityComparer<TDelegate>.Default.Equals(@delegate, default))
             {
                 @delegate = new NotSupportedExceptionDelegateFactory<TDelegate>().Create();
             }
             return @delegate;
         }
+
+        public static TDelegate Compile<TDelegate>(Action<ILGenerator> emitCode) where TDelegate : Delegate => Compile<TDelegate>(emitCode, false);
 
         private static TDelegate CompileUsingDynamicMethod<TDelegate>(Action<ILGenerator> emitCode)
         {
@@ -46,7 +50,7 @@ namespace Platform.Reflection
             var dynamicMethod = new DynamicMethod(GetNewName(), returnType, parameterTypes);
             var generator = dynamicMethod.GetILGenerator();
             emitCode(generator);
-            return (TDelegate)dynamicMethod.CreateDelegate(delegateType);
+            return (TDelegate)(object)dynamicMethod.CreateDelegate(delegateType);
         }
 
         private static TDelegate CompileUsingMethodBuilder<TDelegate>(Action<ILGenerator> emitCode)
@@ -54,19 +58,18 @@ namespace Platform.Reflection
             AssemblyName assemblyName = new AssemblyName(GetNewName());
             var assembly = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
             var module = assembly.DefineDynamicModule(GetNewName());
-            var type = module.DefineType(GetNewName(), attributes);
-
+            var type = module.DefineType(GetNewName());
             var delegateType = typeof(TDelegate);
             var invoke = delegateType.GetMethod("Invoke");
             var returnType = invoke.ReturnType;
             var parameterTypes = invoke.GetParameters().Select(s => s.ParameterType).ToArray();
-
-            MethodBuilder method = type.DefineMethod(GetNewName(), MethodAttributes.Public | MethodAttributes.Static, returnType, parameterTypes);
-            method.SetImplementationFlags(MethodImplAttributes.IL | MethodImplAttributes.AggressiveInlining);
-
+            var methodName = GetNewName();
+            MethodBuilder method = type.DefineMethod(methodName, MethodAttributes.Public | MethodAttributes.Static, returnType, parameterTypes);
+            method.SetImplementationFlags(MethodImplAttributes.IL | MethodImplAttributes.Managed | MethodImplAttributes.AggressiveInlining);
             var generator = method.GetILGenerator();
             emitCode(generator);
-            return (TDelegate)method.CreateDelegate(delegateType);
+            var typeInfo = type.CreateTypeInfo();
+            return (TDelegate)(object)typeInfo.GetMethod(methodName).CreateDelegate(delegateType);
         }
 
         private static string GetNewName() => Guid.NewGuid().ToString("N");
