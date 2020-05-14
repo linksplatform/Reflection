abe9dbf003c5bd33958dfa3774b5697c5d25f599
Konstantin Diachenko <drakonard@gmail.com>
2020-05-14 22:46:09 +0300

    Root page updated to redirect to C# version of documentation.

--- MethodInfoExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-namespace Platform.Reflection
-{
-    public static class MethodInfoExtensions
-    {
-        public static byte[] GetILBytes(this MethodInfo methodInfo) => methodInfo.GetMethodBody().GetILAsByteArray();
-    }
-}
