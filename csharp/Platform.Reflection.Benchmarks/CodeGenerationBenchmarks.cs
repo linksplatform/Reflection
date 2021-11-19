88ac7450c00bdbd4cef34e5fd9acd753ba76fad1
FreePhoenix <freephoenix888@gmail.com>
2021-11-19 14:51:14 +0600

    Remove XML Documentation for tests and benchmarks

--- csharp/Platform.Reflection.Benchmarks/CodeGenerationBenchmarks.cs
+++ csharp/Platform.Reflection.Benchmarks/CodeGenerationBenchmarks.cs
@@ -6,56 +6,18 @@ using BenchmarkDotNet.Attributes;
 
 namespace Platform.Reflection.Benchmarks
 {
-    /// <summary>
-    /// <para>
-    /// Represents the code generation benchmarks.
-    /// </para>
-    /// <para></para>
-    /// </summary>
     [SimpleJob]
     [MemoryDiagnoser]
     public class CodeGenerationBenchmarks
     {
         private class MethodsContainer
         {
-            /// <summary>
-            /// <para>
-            /// The type member method.
-            /// </para>
-            /// <para></para>
-            /// </summary>
             public static readonly Func<int> TypeMemberMethodDelegate = DelegateHelpers.Compile<Func<int>>(EmitCode, typeMemberMethod: false);
-            /// <summary>
-            /// <para>
-            /// The type member method.
-            /// </para>
-            /// <para></para>
-            /// </summary>
             public static readonly Func<int> DynamicMethodDelegate = DelegateHelpers.Compile<Func<int>>(EmitCode, typeMemberMethod: true);
 
-            /// <summary>
-            /// <para>
-            /// Types the member method delegate wrapper.
-            /// </para>
-            /// <para></para>
-            /// </summary>
-            /// <returns>
-            /// <para>The int</para>
-            /// <para></para>
-            /// </returns>
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static int TypeMemberMethodDelegateWrapper() => TypeMemberMethodDelegate();
 
-            /// <summary>
-            /// <para>
-            /// Dynamics the method delegate wrapper.
-            /// </para>
-            /// <para></para>
-            /// </summary>
-            /// <returns>
-            /// <para>The int</para>
-            /// <para></para>
-            /// </returns>
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static int DynamicMethodDelegateWrapper() => DynamicMethodDelegate();
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -67,122 +29,32 @@ namespace Platform.Reflection.Benchmarks
         }
         private static int Function() => 140314;
 
-        /// <summary>
-        /// <para>
-        /// Nots the inlined function.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The int</para>
-        /// <para></para>
-        /// </returns>
         [MethodImpl(MethodImplOptions.NoInlining)]
         private static int NotInlinedFunction() => 140314;
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static int InlinedFunction() => 140314;
 
-        /// <summary>
-        /// <para>
-        /// Types the member method delegate.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The int</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public int TypeMemberMethodDelegate() => MethodsContainer.TypeMemberMethodDelegate();
 
-        /// <summary>
-        /// <para>
-        /// Dynamics the method delegate.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The int</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public int DynamicMethodDelegate() => MethodsContainer.DynamicMethodDelegate();
 
-        /// <summary>
-        /// <para>
-        /// Types the member method delegate wrapper.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The int</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public int TypeMemberMethodDelegateWrapper() => MethodsContainer.TypeMemberMethodDelegateWrapper();
 
-        /// <summary>
-        /// <para>
-        /// Dynamics the method delegate wrapper.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The int</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public int DynamicMethodDelegateWrapper() => MethodsContainer.DynamicMethodDelegateWrapper();
 
-        /// <summary>
-        /// <para>
-        /// Statics the private function with aggressive inlining.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The int</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public int StaticPrivateFunctionWithAggressiveInlining() => InlinedFunction();
 
-        /// <summary>
-        /// <para>
-        /// Statics the private function with no inlining.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The int</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public int StaticPrivateFunctionWithNoInlining() => NotInlinedFunction();
 
-        /// <summary>
-        /// <para>
-        /// Statics the private function.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The int</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public int StaticPrivateFunction() => Function();
 
-        /// <summary>
-        /// <para>
-        /// Bares the constant.
-        /// </para>
-        /// <para></para>
-        /// </summary>
-        /// <returns>
-        /// <para>The int</para>
-        /// <para></para>
-        /// </returns>
         [Benchmark]
         public int BareConstant() => 140314;
     }
--- csharp/Platform.Reflection.Benchmarks/Program.cs
+++ csharp/Platform.Reflection.Benchmarks/Program.cs
@@ -2,20 +2,8 @@ using BenchmarkDotNet.Running;
 
 namespace Platform.Reflection.Benchmarks
 {
-    /// <summary>
-    /// <para>
-    /// Represents the program.
-    /// </para>
-    /// <para></para>
-    /// </summary>
     static class Program
     {
-        /// <summary>
-        /// <para>
-        /// Main.
-        /// </para>
-        /// <para></para>
-        /// </summary>
         static void Main() => BenchmarkRunner.Run<CodeGenerationBenchmarks>();
     }
 }
